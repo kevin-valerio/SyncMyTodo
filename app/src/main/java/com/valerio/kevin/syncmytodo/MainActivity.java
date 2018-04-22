06743a2177555e5d77d24b8c2b47fd2b61067e8d
Kevin Valerio <kevin.valerio@etu.univ-amu.fr>
2018-04-22 23:04:33 +0200

    test

--- app/src/main/java/com/valerio/kevin/syncmytodo/MainActivity.java
+++ app/src/main/java/com/valerio/kevin/syncmytodo/MainActivity.java
@@ -83,6 +83,7 @@ public class MainActivity extends AppCompatActivity
         if (id == R.id.nav_camera) {
             // Handle the camera action
         } else if (id == R.id.nav_gallery) {
+            /*Simple git test*/
 
         } else if (id == R.id.nav_slideshow) {
 
